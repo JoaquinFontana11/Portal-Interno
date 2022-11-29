1d9cdb6174e49b8bab0457491c9eb23aaef1c6ef
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-11-29 12:01:58 -0300

    agregado el form para editar novedades

--- src/routes/(admin)/admin/noveltys/[id]/+page.svelte
+++ src/routes/(admin)/admin/noveltys/[id]/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Plus, Table } from 'svelte-hero-icons';
 	import type { IComponent } from '$lib/types/Components';
 	import AdminForm from '$lib/components/admin/AdminForm.svelte';
-	import AdminButton from '$lib/components/admin/AdminButton.svelte';
 	import { validateInputText, validateEmptyInput } from '$lib/components/inputs/validators';
 	import AdminModalConfirm from '$lib/components/modals/AdminModalConfirm.svelte';
-	import AdminList from '$lib/components/admin/list/AdminList.svelte';
-	import AdminListRowNovelty from '$lib/components/admin/list/rows/AdminListRowNovelty.svelte';
 
 	export let data: PageData;
 	let list = true;
@@ -15,12 +11,15 @@
 	let modalConfirm = false;
 	let messageSubmit = { status: false, message: '' };
 
+	const novelty = JSON.parse(data.novelty);
+	console.log(novelty.date);
+
 	const components: IComponent[] = [
 		{
 			type: 'select',
 			label: 'Paginas',
 			name: 'page_id',
-			value: '',
+			value: novelty.Page.id,
 			required: true,
 			options: JSON.parse(data.pages).map((page) => {
 				return { name: page.title, value: page.id };
@@ -30,14 +29,14 @@
 			type: 'select-image',
 			label: 'Imagen',
 			name: 'image_id',
-			value: '',
+			value: novelty.Image.id,
 			images: JSON.parse(data.images)
 		},
 		{
 			type: 'date',
 			label: 'Fecha de la novedad',
 			name: 'date',
-			value: '',
+			value: new Date(novelty.date),
 			required: true
 		}
 	];
@@ -53,38 +52,27 @@
 			return { status: false, message: 'Alguno de los datos ingresados es incorrecto' };
 		}
 	};
+
+	const addExtraData = (components) => {
+		return [{ name: 'id', value: novelty.id }];
+	};
 </script>
 
-<AdminButton
-	icon={list ? Plus : Table}
-	on:click={() => {
-		list = !list;
-	}}
-/>
 <main class="ml-56 dark:bg-gray-900 bg-neutral-50 h-screen relative overflow-y-scroll">
 	<div class="w-3/4 h-3/4 absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2">
-		{#if list}
-			<AdminList
-				headers={['Titulo', 'Fecha', 'Pagina', 'Imagen']}
-				data={JSON.parse(data.noveltys)}
-				caption="Novedades"
-				actions={['delete', 'edit']}
-				customRow={AdminListRowNovelty}
-			/>
-		{:else}
-			<AdminForm
-				title="Formulario de novedades"
-				{components}
-				submitMessage="Subir novedad"
-				{loading}
-				action="create"
-				{validators}
-				on:validation-end={(e) => {
-					messageSubmit = e.detail;
-					modalConfirm = true;
-				}}
-			/>
-		{/if}
+		<AdminForm
+			title="Formulario de novedades"
+			{components}
+			submitMessage="Modificar novedad"
+			{loading}
+			action="update"
+			{addExtraData}
+			{validators}
+			on:validation-end={(e) => {
+				messageSubmit = e.detail;
+				modalConfirm = true;
+			}}
+		/>
 	</div>
 	<div>
 		{#if modalConfirm}
